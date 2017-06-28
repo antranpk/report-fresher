fd02c2755f7a2a93859979408d49c431ba5ae12c
an tran <antran.pk@gmail.com>
2017-06-28 22:09:17 +0700

    jquery

---
+++ convert/jquery/script.js
@@ -0,0 +1,53 @@
+// var Example;
+	var Example = {
+    	init: function(){
+    		this.scrollToTop();
+        this.disableRightClick();
+        this.acceptRegister();
+        this.blinkText();
+        this.Table.init();
+     	},
+
+      /*Srcoll to the top*/
+     	scrollToTop: function(){
+     		$("#top").on('click', function(){
+          $("html, body").animate({scrollTop: 0}, "slow");
+          return false;
+        });
+     	},
+      disableRightClick: function(){
+        $(".disable-right-click").on("contextmenu", function(e){
+          return false;
+        });
+      },
+      acceptRegister: function(){
+        $("#accept").on('click', function(){
+          if ($("#submitbtn").is(":disabled")) {
+            $("#submitbtn").removeAttr("disabled");
+          } else {
+            $("#submitbtn").attr("disabled", "disabled");
+          }
+        });
+      },
+      blinkText: function(){
+        $(".blink-text").on('click',function(){
+          $(".blink-text p").fadeOut(500);
+          $(".blink-text p").fadeIn(500);        
+        });
+      },
+     	Table: {
+      	init: function(){
+          this.addZebra();
+      	},
+      	addZebra: function(){
+          $(".add-zebra-odd").on("click", function(){
+            $(".table-style table tr:even").removeClass("zebra");
+            $(".table-style table tr:odd").addClass("zebra");
+          });
+          $(".add-zebra-even").on("click", function(){
+            $(".table-style table tr:odd").removeClass("zebra");
+            $(".table-style table tr:even").addClass("zebra");
+          });
+      	}
+     	}
+	};
